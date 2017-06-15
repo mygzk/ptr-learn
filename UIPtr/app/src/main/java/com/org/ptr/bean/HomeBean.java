f48d179d08d7fbae5068d9378a64e13b1c3e5fbc
guozhk <1565619659@qq.com>
2017-06-15 13:21:13 +0800

    src

---
+++ UIPtr/app/src/main/java/com/org/ptr/bean/HomeBean.java
@@ -0,0 +1,34 @@
+package com.org.ptr.bean;
+
+/**
+ * Created by guozhk on 2017/6/15.
+ */
+
+public class HomeBean {
+    private Class cl;
+    private String dre;
+
+    public HomeBean() {
+    }
+
+    public HomeBean(Class cl, String dre) {
+        this.cl = cl;
+        this.dre = dre;
+    }
+
+    public Class getCl() {
+        return cl;
+    }
+
+    public void setCl(Class cl) {
+        this.cl = cl;
+    }
+
+    public String getDre() {
+        return dre;
+    }
+
+    public void setDre(String dre) {
+        this.dre = dre;
+    }
+}
